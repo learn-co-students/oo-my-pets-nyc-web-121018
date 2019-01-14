92d19623b37e223dae7184d7f4d9a8bd2a491226
github username <github email address>
2019-01-13 21:51:43 -0500

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,18 @@
 class Cat
-  # code goes here
+
+  @@all = []
+
+  attr_reader :name
+  attr_accessor :mood
+
+  def initialize(name, mood = "nervous")
+    @name = name
+    @mood = mood
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,19 @@
 class Fish
-  # code goes here
+
+  @@all = []
+
+  attr_reader :name
+  attr_accessor :mood
+
+  def initialize(name, mood = "nervous")
+    @name = name
+    @mood = mood
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+  
 end
